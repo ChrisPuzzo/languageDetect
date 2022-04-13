c91e03703bc99e64cbdde92f7ca5792909c9facb
ChrisPuzzo <cpp1015@wildcats.unh.edu>
2022-04-13 13:14:46 -0400

    added langaugeDectection proper code, which will return the name of the langauge no just the input

--- testDetect.py
+++ testDetect.py
@@ -5,7 +5,7 @@ from langaugeDetect import LanguageCalls
 detectlanguage.configuration.api_key = "8df729e6c77caed3593ca12c940683ee"
 
 
-print(LanguageCalls('漢字').simpleOutput())
+LanguageCalls('漢字').simpleOutput()
 
 
 
