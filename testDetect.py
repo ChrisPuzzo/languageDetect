cd05ee50683b7cc5c1f46c1415245825a09a056e
ChrisPuzzo <cpp1015@wildcats.unh.edu>
2022-04-13 13:27:21 -0400

    minor changes to test file

--- testDetect.py
+++ testDetect.py
@@ -1,9 +1,4 @@
-import detectlanguage
-
 from langaugeDetect import LanguageCalls
-
-detectlanguage.configuration.api_key = "8df729e6c77caed3593ca12c940683ee"
-
 
 kanji = LanguageCalls('漢字').languageDecetion()
 
